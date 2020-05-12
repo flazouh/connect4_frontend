44b5a09b469145d00be497bd3c2d30fcb146e1b0
blabla <alexandredp@apptweak.com>
2020-05-12 18:31:48 +0200

    replaced delete by post

--- static/javascript/ia_player.js
+++ static/javascript/ia_player.js
@@ -68,7 +68,7 @@ class IAPlayer extends Player {
 async function postData(url = '', data = {}) {
     // Default options are marked with *
     const response = await fetch(url, {
-        method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         credentials: 'same-origin', // include, *same-origin, omit
